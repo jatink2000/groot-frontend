c0311746ffcec7f0c77c1999df3b664b0deb338a
jiten <jk0060701@gmail.com>
2025-05-30 10:49:42 +0530

    jhdfgjf

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -9,9 +9,7 @@ function Login() {
             {...logindata,[e.target.name]:e.target.value}
         )
        }
-    
-    
-       console.log(logindata)
+
     return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 login">
