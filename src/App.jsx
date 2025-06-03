8ee631cd8a9a3167048126df2617e2cd74de91b4
jiten <jk0060701@gmail.com>
2025-06-03 10:48:32 +0530

    hlo

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from './Components/Login'
 import Signup from './Components/Signup'
+import Home from './Pages/Home/Home'
+import Password from './Pages/password/Password'
 
 function App() {
 
@@ -14,6 +16,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Login/>}/>
         <Route path='/signup' element={<Signup/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/password' element={<Password/>}/>
+
 
       </Routes>
      </BrowserRouter>
--- src/Pages/password/Password.jsx
+++ src/Pages/password/Password.jsx
@@ -1,65 +1,8 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react'
 
-function Signup() {
-
-
-    let [signdata, setsigndata] = useState([])
-
-    let inputvalue = (e) => {
-        setsigndata(
-            { ...signdata, [e.target.name]: e.target.value }
-        )
-    }
-
-    let go = useNavigate()
-
-
-    //    allusers ---------------
-    let [already, setalready] = useState([])
-    useEffect(() => {
-        allusers()
-    }, [])
-
-    let allusers = () => {
-        axios.get("http://localhost:5000/allusers").then((res) => {
-            if (res.data.status) {
-                setalready(res.data.ourusers)
-            }
-        }).catch((err) => {
-            console.log(err)
-        })
-    }
-
-
-
-    let signupbtn = () => {
-
-        let filterdata = already.filter(item => item.email == signdata.email)
-        let existuser = filterdata[0]
-
-        if (existuser) {
-            alert("already")
-        }
-        else {
-            axios.post("http://localhost:5000/signup", { signdata }).then((res) => {
-                if (res.data.status) {
-                    alert(res.data.msg)
-                    go("/")
-                }
-            }).catch((err) => {
-                console.log(err)
-            })
-        }
-
-
-
-    }
-
-
+function Password() {
     return (
-        <>
+        <div>
             <div className='sign'>
                 <div className="flex flex-1 flex-col justify-center px-6 py-12 lg:px-8 login">
                     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -68,8 +11,8 @@ function Signup() {
                             src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
                             className="mx-auto h-10 w-auto"
                         /> */}
-                        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-                            Sign Up to your account
+                        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-800">
+                            Forgot Your Password
                         </h2>
                     </div>
 
@@ -77,27 +20,10 @@ function Signup() {
                         <div className="space-y-6">
                             <div>
                                 <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
-                                    Username
-                                </label>
-                                <div className="mt-2">
-                                    <input onChange={inputvalue}
-                                        id="text"
-                                        name="Username"
-                                        type="text"
-                                        required
-                                        autoComplete="Username"
-                                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                                    />
-                                </div>
-                            </div>
-
-
-                            <div>
-                                <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
                                     Email address
                                 </label>
                                 <div className="mt-2">
-                                    <input onChange={inputvalue}
+                                    <input
                                         id="email"
                                         name="email"
                                         type="email"
@@ -105,18 +31,36 @@ function Signup() {
                                         autoComplete="email"
                                         className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                     />
-                                    <span id='email-valid' style={{ color: "red", textTransform: "capitalize" }}></span>
                                 </div>
                             </div>
 
                             <div>
                                 <div className="flex items-center justify-between">
                                     <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
-                                        Password
+                                       New Password
                                     </label>
                                 </div>
                                 <div className="mt-2">
-                                    <input onChange={inputvalue}
+                                    <input
+                                        id="password"
+                                        name="password"
+                                        type="password"
+                                        required
+                                        autoComplete="current-password"
+                                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                    />
+                                </div>
+                            </div>
+
+
+                            <div>
+                                <div className="flex items-center justify-between">
+                                    <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                                       Confirm Password
+                                    </label>
+                                </div>
+                                <div className="mt-2">
+                                    <input
                                         id="password"
                                         name="password"
                                         type="password"
@@ -129,18 +73,18 @@ function Signup() {
 
                             <div>
                                 <button
-                                    onClick={signupbtn}
                                     className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                 >
-                                    Sign Up
+                                    Forgot Password
                                 </button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </>
+
+        </div>
     )
 }
 
-export default Signup
+export default Password
